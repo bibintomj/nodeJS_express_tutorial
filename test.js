5221055b70b7c6242f30e979e54b7931d12cbf14
tysonpaul89\n <tyson.paul@digitalmesh.com>
2017-06-23 13:12:08 +0530

    CRUD operation and doc updates

--- test.js
+++ test.js
@@ -1,22 +1,49 @@
 // getting express module
 const express = require('express')
+// Gets the path middleware package
+const path = require('path')
+// Getting ORM package
+const Sequelize = require('sequelize')
+// Getting body parser package
+const bodyParser = require('body-parser')
+// File upload helper
+const expressFileUpload = require('express-fileupload')
+
+// =========================== Package Config ========================
+// Get's the object express framework
 const app = express()
 const port = 4000 // port
 
-// Getting ORM package
-const Sequelize = require('sequelize')
+// Setting directory to serve static content
+app.use(express.static(path.join(__dirname, 'public')))
 
-const ormConnection = new Sequelize(
-    'test',
-    'root',
-    'pass', {
+// Setting view directory
+app.set('views', path.join(__dirname, 'views'))
+
+// Setting view engine
+app.set('view engine', 'ejs')
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Adding file upload middleware to application
+app.use(expressFileUpload())
+
+// =========================== ORM Config ============================
+// Sequelize package database configuration
+const ormObj = new Sequelize(
+    'test', // database name
+    'root', // db Username
+    'pass', { // db Password
+      dialect: 'mysql', // Specifying DBMS
       logging: false // Disables console logging queries
     }
 )
 
-const User = ormConnection.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
+// User Model definition
+const User = ormObj.define('user', { // Table name
+  id: { // field name
+    type: Sequelize.INTEGER, // field type for INT
     primaryKey: true,
     autoIncrement: true
   },
@@ -35,21 +62,21 @@ const User = ormConnection.define('user', {
   doj: {
     type: Sequelize.DATE,
     allowNull: false
+  },
+  profile: {
+    type: Sequelize.STRING,
+    allowNull: true
   }
 }, {
-  timestamps: false,
-  freezeTableName: true
+  freezeTableName: true, // To user original name of table
+  timestamps: false // Prevents sequelize from creating default columns
 })
 
-// Setting view directory
-app.set('views', './views')
-
-// Setting view engine
-app.set('view engine', 'ejs')
-
+// =========================== Routes ================================
 // Home page route
 app.get('/', function (req, res, next) {
-  res.send('<h1>Homepage</h1>')
+  app.locals.pageTitle = 'Home'
+  res.render('home')
 })
 
 // About page route
@@ -66,21 +93,135 @@ app.get('/hello/:name', function (req, res, next) {
   res.render('hello', { name: name })
 })
 
-// User listing route
-app.get('/user/list', function (req, res, next) {
-  // Code
-})
-
-// User Add form route
+// =========================== CURD Routes ===========================
+// ___________________________ Create User Operation _________________
 app.get('/user/add', function (req, res, next) {
+  req.app.locals.pageTitle = 'Add User'
   res.render('add')
 })
 
-// User Add form processing route
 app.post('/user/add', function (req, res, next) {
-// Code
+  // gets POST body
+  let formData = req.body
+
+  // Gets uploaded image object
+  let imageData = req.files.image
+
+  // Uploading image to /public/uploads directory if present
+  if (imageData) {
+    imageData.mv(
+      path.join(__dirname, './public/uploads/', imageData.name),
+      function (err) {
+        if (err) {
+          next(new Error(err))
+        }
+      }
+    )
+  }
+
+  // Save data to database
+  // following create function is equivalent to
+  // INSERT INTO user(name, age, dob, doj) VALUES ('joe', 25, ...)
+  User.create({
+    name: formData.name,
+    age: formData.age,
+    dob: formData.dob,
+    doj: Sequelize.fn('NOW'),
+    profile: imageData ? imageData.name : null
+  }).catch(err => {
+    if (err) next(new Error(err))
+  })
+
+  res.redirect('/user/list')
 })
 
+// ___________________________ List User Operation ___________________
+app.get('/user/list', function (req, res, next) {
+  // setting page header
+  req.app.locals.pageTitle = 'List User'
+
+  // gets all user details from database
+  // following findAll function is equivalent to
+  // SELECT id, name, age, profile FROM user
+  User.findAll({
+    attributes: ['id', 'name', 'age', 'profile']
+  }).then(results => {
+    // Render the view page
+    res.render('list', {userData: results})
+  }).catch(err => {
+    if (err) next(new Error(err))
+  })
+})
+
+// ___________________________ Update User Operation _________________
+app.get('/user/add/:userId', function (req, res, next) {
+  // Gets the ID of the user from URL
+  let userId = req.params.userId
+  // Setting page heading
+  res.locals.pageTitle = 'Edit User'
+
+  // gets the user data
+  User.findOne({
+    where: {
+      id: userId
+    }
+  }).then(result => {
+    res.render('add', {
+      userData: result
+    })
+  }).catch(err => {
+    if (err) next(new Error(err))
+  })
+})
+
+app.post('/user/edit', function (req, res, next) {
+  // gets the form data
+  let formData = req.body
+
+  // Gets uploaded image object
+  let imageData = req.files.image
+
+  let userData = { // value object
+    name: formData.name,
+    age: formData.age,
+    dob: formData.dob
+  }
+
+  // Uploading image to /public/uploads directory if present
+  if (imageData) {
+    userData.profile = imageData.name
+    imageData.mv(
+      path.join(__dirname, './public/uploads/', imageData.name),
+      function (err) {
+        if (err) {
+          next(new Error(err))
+        }
+      }
+    )
+  }
+
+  // Updates user data
+  User.update(userData, { // Option object
+    where: {
+      id: formData.id
+    }
+  }).then(updRes => res.redirect('/user/list')).catch(err => {
+    if (err) next(new Error(err))
+  })
+})
+
+// ___________________________ Delete User Operation _________________
+app.get('/user/delete/:userId', function (req, res, next) {
+  // deletes user data of the given user ID
+  User.destroy({
+    where: {
+      id: req.params.userId
+    }
+  }).then(delRes => res.redirect('/user/list')).catch(err => {
+    if (err) next(new Error(err))
+  })
+})
+// =========================== Error Routes ==========================
 // Error page route
 app.get('/error', function (req, res, next) {
   // passing a custom error to error handler route
@@ -99,10 +240,14 @@ app.use(function (err, req, res, next) {
   console.error(err.stack)
   res.status(500)
   // shows error message
-  res.send('<h1>Server Error</h1> <br/> Error Message: <b>' + err.message + '</b>')
+  res.send(`<h1>Server Error</h1> <br/>
+    Error Message: <b>${err.message}</b>`
+  )
 })
 
+// =========================== Server Setup ==========================
 app.listen(port, function () {
-  console.log('Express started on http://localhost:' +
-  port + '; press Ctrl-C to terminate.')
+  console.log(`Express started on http://localhost:'${port};
+  press Ctrl-C to terminate.`
+  )
 })
